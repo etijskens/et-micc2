b5efafc94d3be69194a24aae0a1a47b8290dd05e
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2021-04-02 13:09:35 +0200

    Update check_environment.py

--- et_micc2/check_environment.py
+++ et_micc2/check_environment.py
@@ -8,7 +8,7 @@ This submodule implements the ``micc check`` command.
 It checks the environment for Python packages and tools micc2 depends on.
 """
 
-import os
+import os,sys
 import subprocess
 import importlib
 from importlib.metadata import version
@@ -25,7 +25,7 @@ def check_tool(tool,local):
         return False
     else:
         completed_version = subprocess.run([tool, '--version'], capture_output=True, text=True)
-        version_string = completed_version.stdout.strip().replace('\n','\n        ')
+        version_string = completed_version.stdout.strip().replace('\n\n','\n').replace('\n','\n        ')
         if local:
             click.secho(f'    {tool} is available:', fg='green')
             print(f'      {version_string}')
@@ -54,13 +54,15 @@ def check_cmd(options):
     
     where = os.environ['VSC_INSTITUTE_CLUSTER'] if 'VSC_HOME' in os.environ else 'local'
     local = where=='local'
-    
-    
+
     print('Python\n'
           '------')
     completed_which = subprocess.run(['which', 'python'],text=True,capture_output=True)
-    which_string = completed_which.stdout.replace('\n', '\n      ')
-    print(f'python={which_string}')
+    which_string = completed_which.stdout.replace('\n', '')
+
+    print(f'python = {which_string}')
+    python_version = sys.version.replace('\n', ' ')
+    print(f'version= {python_version}')
     if not local:
         # check that we are not using the system Python:
         if '/usr/bin/' in which_string:
@@ -71,7 +73,6 @@ def check_cmd(options):
     pip_install         = '    To install it, run `pip install {module_name}` in your environment.'
     pip_install_cluster = '    To install it, run `PYTHONUSERBASE=$VSC_DATA/.local/ python -m pip install --user {module_name}`'
     load_module = '    Load a cluster module containing {module_name}.'
-
 
     can_build_doc = True
     can_build_cpp = True
@@ -90,8 +91,8 @@ def check_cmd(options):
               ,'click'              : '7.0'
               ,'pytest'             : '5.0'
               }
-    print('Python packages')
-    print('---------------')
+    print('\nPython packages'
+          '\n---------------')
     for module_name, version_needed in modules.items():
         try:
             m = importlib.import_module(module_name)
@@ -164,19 +165,23 @@ def check_cmd(options):
         can_build_cpp = False
 
     # compilers
-    print('- Compilers:')
+    print('\n- Compilers:')
     check_tool('g++', local)
+    print()
     check_tool('icpc', local)
+    print()
     check_tool('gcc', local)
+    print()
     check_tool('gfortran', local)
+    print()
     check_tool('ifort', local)
 
     print('\nYour environment is ready to:')
     print('  - use poetry (e.g. `poetry publish --build`):', 'YES' if can_poetry    else 'NO')
     print('  - use pytest for automating tests           :', 'YES' if can_pytest    else 'NO')
-    print('  - build binary extension from C++           :', 'if C++ compiler available' if can_build_cpp else 'NO')
-    print('  - build binary extension from Fortran       :', 'if Fortran and C compiler available' if can_build_f90 else 'NO')
+    print('  - build binary extensions from C++          :', 'if a C++ compiler is available' if can_build_cpp else 'NO')
+    print('  - build binary extensions from Fortra       :', 'if a Fortran and a C compiler are available' if can_build_f90 else 'NO')
     print('  - build command line interfaces with click  :', 'YES' if can_build_cli else 'NO')
     print('  - generate documentation with sphinx        :', 'YES' if can_build_doc else 'NO')
-    print('  - git                                       :', 'YES' if can_git else 'NO')
-    print('  - create remote repositories at github.com  :', 'YES' if can_gh else 'NO')
+    print('  - git                                       :', 'YES' if can_git       else 'NO')
+    print('  - create remote repositories at github.com  :', 'YES' if can_gh        else 'NO')
