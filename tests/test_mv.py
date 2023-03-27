2dba65b3ed436584cb0439bca9dc50cf160c7867
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2023-03-27 18:13:53 +0200

    Update test_mv.py

--- tests/test_mv.py
+++ tests/test_mv.py
@@ -58,6 +58,48 @@ def test_rename_remove_1():
 def test_rename_remove_2():
     """test rename and remove of sub-submodules"""
     with utils.in_directory(helpers.test_workspace()):
+        results = []
+        # Create package BAR
+        results.append(helpers.micc(['-p', 'BAR', 'create', '--allow-nesting', '--remote=none']))
+        assert Path('BAR/bar/__init__.py').exists()
+        # add Python submodule 'foo'
+        results.append(helpers.micc(['-p', 'BAR', 'add', 'foo', '--py']))
+        assert (Path('.') / 'BAR' / 'bar' / 'foo').is_dir()
+        # add Python submodule 'foo/soup'
+        results.append(helpers.micc(['-p', 'BAR', 'add', 'foo/soup', '--py']))
+        assert (Path('.') / 'BAR' / 'bar' / 'foo'/ 'soup').is_dir()
+        # rename
+        results.append(helpers.micc(['-p', 'BAR', 'mv', 'foo/soup', 'onion_soup']))
+        assert (Path('.') / 'BAR' / 'bar' / 'foo'/ 'onion_soup').is_dir()
+        # remove
+        results.append(helpers.micc(['-p', 'BAR', 'mv', 'foo/onion_soup']))
+        assert not (Path('.') / 'BAR' / 'bar' / 'foo'/ 'onion_soup').is_dir()
+
+
+def test_rename_remove_3():
+    """test rename and remove of submodules with sub-submodules"""
+    with utils.in_directory(helpers.test_workspace()):
+        results = []
+        # Create package BAR
+        results.append(helpers.micc(['-p', 'BAR', 'create', '--allow-nesting', '--remote=none']))
+        assert Path('BAR/bar/__init__.py').exists()
+        # add Python submodule 'foo'
+        results.append(helpers.micc(['-p', 'BAR', 'add', 'foo', '--py']))
+        assert (Path('.') / 'BAR' / 'bar' / 'foo').is_dir()
+        # add Python submodule 'foo/soup'
+        results.append(helpers.micc(['-p', 'BAR', 'add', 'foo/soup', '--py']))
+        assert (Path('.') / 'BAR' / 'bar' / 'foo'/ 'soup').is_dir()
+        # rename bar/foo/soup -> bar/foo2/soup
+        results.append(helpers.micc(['-p', 'BAR', 'mv', 'foo', 'foo2']))
+        assert (Path('.') / 'BAR' / 'bar' / 'foo2'/ 'soup').is_dir()
+        # remove bar/foo2 and bar/foo2/soup
+        results.append(helpers.micc(['-p', 'BAR', 'mv', 'foo2']))
+        assert not (Path('.') / 'BAR' / 'bar' / 'foo2'/ 'soup').is_dir()
+        assert not (Path('.') / 'BAR' / 'bar' / 'foo2').is_dir()
+
+def test_move_1():
+    """test rename and remove of sub-submodules"""
+    with utils.in_directory(helpers.test_workspace()):
         component_flags = ['--py', '--f90', '--cpp']
         results = []
         # Create package BAR
@@ -78,7 +120,7 @@ def test_rename_remove_2():
 
 
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_rename_remove_1
+    the_test_you_want_to_debug = test_rename_remove_3
 
     print(f"{__file__}::__main__ executing test '{the_test_you_want_to_debug}'")
     the_test_you_want_to_debug()
