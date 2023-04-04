a4da71e3d3d2cf0eedf262fa4c898e846bcd8925
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2023-04-04 13:06:43 +0200

    Commit prior to `micc2 mv foo/soup onion_soup`

--- et_micc2/subcmds/mv.py
+++ et_micc2/subcmds/mv.py
@@ -190,8 +190,8 @@ def mv_move(project, component_traits):
         if key == 'context':
             context = component_traits.db_entry[key]
             context['add_name'] = str(Path(component_traits.to) / component_traits.name)
-            context['module_srcdir'] = context['add_name'].replace(os.sep, '.')
-            context['module_srcdir'] = context['add_name'].replace(os.sep, '.')
+            context['module_srcdir'] = Path(context['package_name']) / context['add_name']
+            context['import_lib'] = context['module_srcdir'].replace(os.sep, '.')
         else:
             path = project.context.project_path / key
             parent_folder, filename, old_string = path.parent, path.name, val
